15b5badccb8a531494af02ac7fa61a67e30b5bff
Mustafa Basit <mailtobasit74@gmail.com>
2020-07-07 20:29:56 +0530

    Integration with Smooth-ui

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,14 +1,26 @@
 (ns app.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [app.theme :refer [cheffy-theme]]
+            ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
 
 (defn app
   []
-  [:div "Cheffy"])
+  [:<> 
+   [:> Normalize]
+   [:> Button "Click Here"]
+   [:> Button "Submit"]
+   [:hr]
+   [:div "Hello World"]
+   [:hr]
+   [:> ThemeProvider {:theme cheffy-theme}
+    [:<>
+     [:> Button "Download"]
+     [:> Button "Login"]]]])
 
 (defn ^:dev/after-load start
   []
   (r/render [app]
-    (.getElementById js/document "app")))
+            (.getElementById js/document "app")))
 
 (defn ^:export init
   []
