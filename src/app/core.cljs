dc9763a3fd4b388ea0327d6409f94dd13b764dc1
Mustafa Basit <mailtobasit74@gmail.com>
2020-07-18 13:01:57 +0530

    Added Auth/profiles

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -5,6 +5,8 @@
 
             ;; -- auth --
             [app.auth.views.profile :refer [profile]]
+            [app.auth.views.sign-up :refer [sign-up]]
+            [app.auth.views.log-in :refer [log-in] ]
             
             ;; -- become-a-chef
             [app.become-a-chef.views.become-a-chef :refer [become-a-chef]]
@@ -28,6 +30,8 @@
   [page-name]
   (case page-name
     :profile [profile]
+    :sign-up [sign-up]
+    :log-in [log-in]
     :become-a-chef [become-a-chef]
     :inboxes [inboxes]
     :recipes [recipes]
--- src/app/nav/views/public.cljs
+++ src/app/nav/views/public.cljs
@@ -1,7 +1,36 @@
 (ns app.nav.views.public
-  (:require [reagent.core :as r]
+  (:require [re-frame.core :as rf]
+            [app.nav.views.nav-item :refer [nav-item]]
             ["@smooth-ui/core-sc" :refer [Box]]))
 
-(defn public 
+(defn public
   []
-  "public")
+  (let [active-nav @(rf/subscribe [:active-nav])
+        nav-items [{:id :recipes
+                    :name "recipes"
+                    :href "#recipes"
+                    :dispatch #(rf/dispatch [:set-active-nav :recipes])}
+                   {:id :become-a-chef
+                    :name "chef"
+                    :href "#become-a-chef"
+                    :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
+                   {:id :sign-up
+                    :name "sign-up"
+                    :href "#sign-up"
+                    :dispatch #(rf/dispatch [:set-active-nav :sign-up])}
+                   {:id :login
+                    :name "login"
+                    :href "#login"
+                    :dispatch #(rf/dispatch [:set-active-nav :log-in])}]]
+
+    [:> Box {:display "flex"
+             :justify-content "flex-end"
+             :py 1}
+
+     (for [{:keys [id name href dispatch]} nav-items]
+       [nav-item {:key id
+                  :id id
+                  :name name
+                  :href href
+                  :dispatch dispatch
+                  :active-nav active-nav}])]))
