183a1f46d602916553e395175529702b8b896407
Mustafa Basit <mailtobasit74@gmail.com>
2020-07-19 23:18:41 +0530

    Log-in form Done - check (swap! values  assoc :email (.. % -target -value))

--- src/app/auth/views/log_in.cljs
+++ src/app/auth/views/log_in.cljs
@@ -1,6 +1,38 @@
 (ns app.auth.views.log-in
-  (:require [app.components.page-nav :refer [page-nav]]))
+  (:require [re-frame.core :as rf]
+            [reagent.core :as r]
+            ["@smooth-ui/core-sc" :refer [Box Row Col FormGroup Label Input Button ]]
+            [app.components.page-nav :refer [page-nav]]))
 
 (defn log-in
   []
-  [page-nav {:center "Log in"}])
+  (let [initial-values {:email "" :password ""}
+        values (r/atom initial-values)]
+    (fn []
+      [:> Row {:justify-content "center"}
+       [:> Col {:xs 12 :sm 6}
+        [page-nav {:center "Log in"}]
+        [:> FormGroup
+         [:> Label {:html-for :email} "Email"]
+         [:> Input {:control true
+                    :id :email
+                    :type "email"
+                    :value (:email @values)
+                    :on-change #(swap! values assoc :email (.. % -target -value))}]]
+        [:> FormGroup
+         [:> Label {:html-for :password} "Password"]
+         [:> Input {:control true
+                    :id :password
+                    :type "password"
+                    :value (:password @values)
+                    :on-change #(swap! values assoc :password (.. % -target -value))}]]
+        [:> Box {:display "flex"
+                 :justify-content "space-between"}
+         [:> Box {:py 1
+                  :pr 2}
+          [:a {:href "#sign-up"
+               :on-click #(rf/dispatch [:set-active-nav :sign-up])}
+           "New to Cheffy? Create an account!"]]
+         [:> Box
+          [:> Button {:on-click #(js/console.log "log-in")}
+           "Log in"]]]]])))
--- src/app/core.cljs
+++ src/app/core.cljs
@@ -51,10 +51,10 @@
 
 (defn ^:dev/after-load start
   []
-  (rf/dispatch-sync [:initialize-db])
   (r/render [app]
             (.getElementById js/document "app")))
 
 (defn ^:export init
   []
+  (rf/dispatch-sync [:initialize-db])
   (start))
